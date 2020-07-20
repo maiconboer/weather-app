f1a1be8c328872671b8b81710f835eb086247a23
Maicon Boer <maicon_boer@hotmail.com>
2020-07-20 15:49:18 -0300

    environment variables

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 export async function getDataCity(value) {
   let result = {}
-
+  
   try {
-    await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&appid=1cbf440408df24d6d38f840c33a5e908&lang=pt_br`)
+    await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&appid=${process.env.REACT_APP_API_KEY1}&lang=pt_br`)
     .then(response => {
       result = response.data;
   })
@@ -18,7 +18,7 @@ export async function getForecastNextDays(value) {
   let resultForecast = {}
 
   try {
-    await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${value}&units=metric&appid=e7f38425e5ffe30ed04f95a04e3eb292&lang=pt_br`)
+    await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${value}&units=metric&appid=${process.env.REACT_APP_API_KEY2}&lang=pt_br`)
     .then(response => {
       resultForecast = response.data;
   })
